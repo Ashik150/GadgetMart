07956b32a8f7dd463e892a65d09a3d13f3eae758
Ashik150 <sskhan4409@gmail.com>
2025-01-04 12:34:10 +0600

    Payment page bug fixed

--- frontend/src/components/Payment/Payment.jsx
+++ frontend/src/components/Payment/Payment.jsx
@@ -227,7 +227,7 @@ const PaymentInfo = ({
                         ) : null}
                     </div>
                     <h4 className="text-[18px] pl-2 font-[600] text-[#000000b1]">
-                        Pay with Debit/credit card
+                        Pay with Debit/Credit card
                     </h4>
                 </div>
 
@@ -419,21 +419,21 @@ const CartData = ({ orderData }) => {
     return (
         <div className="w-full bg-[#fff] rounded-md p-5 pb-8">
             <div className="flex justify-between">
-                <h3 className="text-[16px] font-[400] text-[#000000a4]">subtotal:</h3>
-                <h5 className="text-[18px] font-[600]">${orderData?.subTotalPrice}</h5>
+                <h3 className="text-[16px] font-[400] text-[#000000a4]">Subtotal:</h3>
+                <h5 className="text-[18px] font-[600]">{orderData?.subTotalPrice} BDT</h5>
             </div>
             <br />
             <div className="flex justify-between">
-                <h3 className="text-[16px] font-[400] text-[#000000a4]">shipping:</h3>
-                <h5 className="text-[18px] font-[600]">${shipping}</h5>
+                <h3 className="text-[16px] font-[400] text-[#000000a4]">Shipping:</h3>
+                <h5 className="text-[18px] font-[600]">{shipping} BDT</h5>
             </div>
             <br />
             <div className="flex justify-between border-b pb-3">
                 <h3 className="text-[16px] font-[400] text-[#000000a4]">Discount:</h3>
-                <h5 className="text-[18px] font-[600]">{orderData?.discountPrice ? "$" + orderData.discountPrice : "-"}</h5>
+                <h5 className="text-[18px] font-[600]">{orderData?.discountPrice ? orderData.discountPrice : "-"} BDT</h5>
             </div>
             <h5 className="text-[18px] font-[600] text-end pt-3">
-                ${orderData?.totalPrice}
+                {orderData?.totalPrice} BDT
             </h5>
             <br />
         </div>
--- frontend/src/pages/ProductDetailsPage.jsx
+++ frontend/src/pages/ProductDetailsPage.jsx
@@ -2,28 +2,41 @@ import React, { useEffect, useState } from 'react'
 import Header from '../components/Layout/Header'
 import ProductDetails from '../components/Products/ProductDetails'
 import Footer from '../components/Layout/Footer'
-import { useParams } from 'react-router-dom'
+import { useParams,useSearchParams } from 'react-router-dom'
 import { productData } from '../static/data'
 import { useSelector } from 'react-redux'
 import SuggestedProduct from '../components/Products/SuggestedProduct'
 import { useDispatch } from 'react-redux'
 import { getAllProducts } from '../redux/actions/product'
+import { getAllEvents } from '../redux/actions/event'
 
 const ProductDetailsPage = () => {
   const dispatch = useDispatch();
   const { allProducts } = useSelector((state) => state.products);
-  console.log("Products are", allProducts);
+  const { allEvents } = useSelector((state) => state.events);
+  //console.log("Products are", allProducts);
   const { id } = useParams();
   const [data, setData] = useState(null);
+  const [searchParams] = useSearchParams();
+  const eventData = searchParams.get("isEvent");
 
   useEffect(() => {
+    if(eventData!==null){
+      dispatch(getAllEvents());
+    }else{
     dispatch(getAllProducts());
-  }, [dispatch]);
+    }
+  }, [dispatch,eventData]);
 
   useEffect(() => {
-    const data2 =allProducts && allProducts.find((i) => i._id === id);
-    setData(data2);
-  }, [allProducts, id]);
+    if (eventData !== null) {
+      const event = allEvents && allEvents.find((i) => i._id === id);
+      setData(event);
+    } else {
+      const product = allProducts && allProducts.find((i) => i._id === id);
+      setData(product);
+    }
+  }, [allProducts, allEvents, id, eventData]);
   return (
     <div>
       <Header />
