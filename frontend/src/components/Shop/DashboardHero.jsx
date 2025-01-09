53b3b43c0f1c117779997fab9cf41806d81f1bce
Ashik150 <sskhan4409@gmail.com>
2025-01-09 13:34:42 +0600

    withdraw-money Route change

--- frontend/src/components/Shop/DashboardHero.jsx
+++ frontend/src/components/Shop/DashboardHero.jsx
@@ -102,7 +102,7 @@ const DashboardHero = () => {
             </h3>
           </div>
           <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">{availableBalance} BDT</h5>
-          <Link to="/dashboard-withdraw-money">
+          <Link to="/shopdashboard-withdraw-money">
             <h5 className="pt-4 pl-[2] text-[#077f9c]">Withdraw Money</h5>
           </Link>
         </div>
