7d4ae2fb08cafd80e0ceb9293d06c60128b0f0b4
Ayonspoiler <ayon4052@gmail.com>
2025-03-19 02:01:05 +0600

    product route updated

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {createProduct,getProducts,deleteProduct,getAllProduct,createNewReview} from '../controllers/product.controller.js';
+import {createProduct,getProducts,deleteProduct,getAllProduct,createNewReview, getBestSellingProducts} from '../controllers/product.controller.js';
 const router = express.Router();
 import { verifyToken } from '../middleware/verifyToken.js';
 
@@ -8,5 +8,7 @@ router.get('/get-all-products-shop/:id',getProducts);
 router.delete('/delete-shop-product/:id',deleteProduct);
 router.get('/get-all-products',getAllProduct);
 router.put('/create-new-review',verifyToken,createNewReview);
+router.get("/get-best-selling-products", getBestSellingProducts);
+
 
 export default router;
